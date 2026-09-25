ff7717ffc96f253601217efde57b176b3dbf4a3b
Raliat Babatunde <raliatbabatunde1@gmail.com>
2026-09-25 09:03:56 +0100

    Final - add validation and active route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.use(express.json()); // Parse JSON bodies
 
 let todos = [
   { id: 1, task: 'Learn Node.js', completed: false },
-  { id: 2, task: 'Build CRUD API', completed: false },
+  { id: 2, task: 'Build CRUD API', completed: false }
 ];
 
 // GET All – Read
@@ -12,8 +12,28 @@ app.get('/todos', (req, res) => {
   res.status(200).json(todos); // Send array as JSON
 });
 
+
+// 2. Get active - MUST come BEFORE :id
+app.get('/todos/active', (req, res) => {
+  const activeTodos = todos.filter(t => !t.completed);
+  res.json(activeTodos);
+});
+
+// 3. Get single - MUST come AFTER active
+app.get('/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id); // important!
+  const todo = todos.find(t => t.id === id);
+  if (!todo) {
+    return res.status(404).json({ error: "Todo not found" });
+  }
+  res.json(todo);
+});
+
 // POST New – Create
 app.post('/todos', (req, res) => {
+  if (!req.body.task) {
+    return res.status(400).json({ error: '"task" field is required'});
+  }
   const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
   todos.push(newTodo);
   res.status(201).json(newTodo); // Echo back
@@ -32,15 +52,14 @@ app.delete('/todos/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const initialLength = todos.length;
   todos = todos.filter((t) => t.id !== id); // Array.filter() – non-destructive
-  if (todos.length === initialLength)
+  if (todos.length === initialLength){
     return res.status(404).json({ error: 'Not found' });
+  }
+    
   res.status(204).send(); // Silent success
 });
 
-app.get('/todos/completed', (req, res) => {
-  const completed = todos.filter((t) => t.completed);
-  res.json(completed); // Custom Read!
-});
+
 
 app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Server error!' });
